e0121224bee0a19aeddf05209ebfda47380bb8a9
Adam Whitehurst <Adam.Whitehurst@live.com>
2017-03-26 10:45:48 -0700

    Added more logic and stepping functionality

--- PixiPocketKnife/www/scripts/grid.js
+++ PixiPocketKnife/www/scripts/grid.js
@@ -6,7 +6,7 @@ var nodeWidth = 0;
 var nodeHeight = 0;
 var stage;
 var renderer;
-var grid;
+var array;
 //Pixi Aliases
 var Container = PIXI.Container,
     autoDetectRenderer = PIXI.autoDetectRenderer,
@@ -21,11 +21,6 @@ var Container = PIXI.Container,
     }
 
     function init() {
-        grid = new Array(COLUMNS);
-        for (var i = 0; i < COLUMNS; i++) {
-            grid[i] = new Array(ROWS);
-        };
-
         //Find where to hook Pixi's canvas to
         var canvasHook = document.getElementById('canvas-hook');
         var bounds = canvasHook.getBoundingClientRect();
@@ -58,6 +53,7 @@ var Container = PIXI.Container,
 })();
 //Grid Node Stuff
 var GridState = {
+    NONE: 0xFFFFFF,
     START: 0xAA3939,
     END: 0x2D882D,
     BLOCKED: 0x003333,
@@ -71,36 +67,91 @@ class GridNode {
         this.gridPosJ = j;
         this.worldPosX = width * i;
         this.worldPosY = height * j;
-        this.gScore = 0;
-        this.fScore = 0;
+        this.gScore = -1;
+        this.fScore = -1;
         this.hScore = 0;
         this.sprite = new PIXI.Sprite(resources["/res/icons/android/icon-96-xhdpi.png"].texture);
         this.sprite.x = this.worldPosX;
         this.sprite.y = this.worldPosY;
         this.sprite.width = width;
         this.sprite.height = height;
-    }
-}
+        this._state = GridState.NONE;
+        this.fromNode = undefined; // The previous node when performing A* search. Change to array in a_star?
+        this.neighbors = [];
+    };
 
-/**
- * @param {GridNode} node The node to change the state of.
- * @param {GridState} state The state to change the node to.
- */
-function setNodeState(node, state) {
-    if (!(node instanceof GridNode)) throw Error("Node parameter not an instance of GridNode!");
-    node.sprite.tint = state;
-}
+     /**
+     * @param {GridState} s The state to change the node to.
+     */
+    setNodeState (s) {
+        this._state = s;
+        this.sprite.tint = s;
+        renderer.render(stage);
+    };
+
+    /**
+     * Returns the node's current grid state
+     */
+    getNodeState () {
+        return this._state;
+    };
+
+    /**
+     * Push the passed node into neighbors array (does not verify that they are actually neighbors)
+     * @param {GridNode} node
+     */
+    pushNeighbor(node) {
+        this.neighbors.push(node);
+    };
+
+    /**
+     * Finds Manhattan distance between this node and toNode
+     * @param {GridNode} toNode
+     */
+    calcHScore(toNode) {
+        var a = toNode.worldPosX - this.worldPosX;
+        var b = toNode.worldPosY - this.worldPosY;
+
+        this.hScore = Math.abs(a) + Math.abs(b);
+    };
+};
 
 function initGrid(w, h) {
-    console.log("Initialize the grid!");
     nodeWidth = w / COLUMNS;
     nodeHeight = h / ROWS;
 
+    this.array = new Array(COLUMNS);
+    for (var i = 0; i < COLUMNS; i++) {
+        array[i] = new Array(ROWS);
+    };
+
     for (var i = 0; i < COLUMNS; i++) {
         for (var j = 0; j < ROWS; j++) {
-            grid[i][j] = new GridNode(i, j, nodeWidth, nodeHeight);
-            stage.addChild(grid[i][j].sprite);
+            this.array[i][j] = new GridNode(i, j, nodeWidth, nodeHeight);
+            stage.addChild(this.array[i][j].sprite);
+            renderer.render(stage);
+        }
+    }
+
+    // wtf ...
+    //for every node in the grid
+    for (var i = 0; i < COLUMNS; i++) {
+        for (var j = 0; j < ROWS; j++) {
+            //for each adjacent node (including diagonals)
+            for (var neighborI = i - 1; neighborI <= i + 1; neighborI++) {
+                for (var neighborJ = j - 1; neighborJ <= j + 1; neighborJ++) {
+                    //if the indeces exist in the corresponding array and aren't 0,0
+                    if (neighborI in this.array) {
+                        if (neighborJ in this.array[i]) {
+                            if (!(neighborI == i && neighborJ == j)) {
+                                this.array[i][j].neighbors.push(this.array[neighborI][neighborJ]);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     renderer.render(stage);
+    return this;
 }
--- PixiPocketKnife/www/scripts/pixi.js
+++ PixiPocketKnife/www/scripts/pixi.js
@@ -1,17 +1,46 @@
 ﻿var width;
 var height;
-var grid;
+var gridInitted;
+var aStarInitted;
 (function () {
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
     function onDeviceReady() {
         var bounds = document.getElementById('canvas-hook').getBoundingClientRect();
         this.width = bounds.width;
         this.height = bounds.height;
-        var restartBtn = document.getElementById('initialize-btn');
-        restartBtn.addEventListener('click', onInitializeClick.bind (this), false);
+        var initBtn = document.getElementById('initialize-btn');
+        initBtn.addEventListener('click', runToEnd.bind(this), false);
+
+        var rsrtBtn = document.getElementById('restart-btn');
+        rsrtBtn.addEventListener('click', onRestartClick.bind(this), false);
+
+        var stepBtn = document.getElementById('step-btn');
+        stepBtn.addEventListener('click', onStepClick.bind(this), false);
     }
 })();
 function onInitializeClick() {
+    console.log("Initialize a grid!");
     this.grid = initGrid(width, height);
-    console.log("Done.");
+};
+function onRestartClick() {
+    if (!this.grid) {
+        onInitializeClick();
+    }
+    console.log("Begin A* Pathfinder!");
+    initAStar(grid.array[0][0], grid.array[COLUMNS - 1][ROWS - 1]);
+    aStarInitted = true;
+};
+
+function onStepClick() {
+    if (!this.aStarInitted) {
+        onRestartClick();
+    }
+    console.log("Step.");
+    stepAStar();
+};
+
+function runToEnd() {
+    while (!aStarDone) {
+        onStepClick();
+    }
 }
