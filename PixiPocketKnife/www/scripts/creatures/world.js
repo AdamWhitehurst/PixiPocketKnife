9e0fe1c674bc418b26c0e2af85ee9dfc2b0a2fdf
Adam Whitehurst <Adam.Whitehurst@live.com>
2017-04-09 15:17:57 -0700

    Gutted vector-based flocking, added info panel to header

--- PixiPocketKnife/www/scripts/creatures/world.js
+++ PixiPocketKnife/www/scripts/creatures/world.js
@@ -31,7 +31,7 @@ World.prototype = {
         this.populate();
 
         // Specific the starting state 
-        this.state = this.loop;
+        this.state = this.worldUpdateLoop;
         this.renderer.render(this.stage);
 
         // Set up game loop ticker
@@ -61,36 +61,17 @@ World.prototype = {
             var y = Math.random() * this.renderer.view.height;
 
             this.creatures[i] = new Creature(this, x, y);
-            this.creatures[i].velocity.random();
         }
     },
 
-    loop: function () {
-
+    worldUpdateLoop: function () {
         if (!this.creatures)
             this.populate();
 
         // update each creature
         var creatures = this.creatures;
         creatures.forEach(function (creature) {
-            // move
-            var input = [];
-            for (var i in creatures) {
-                input.push(creatures[i].location.x);
-                input.push(creatures[i].location.y);
-                input.push(creatures[i].velocity.x);
-                input.push(creatures[i].velocity.y);
-            }
-            var output = creature.network.activate(input);
-            creature.moveTo(output);
-
-            // learn
-            var learningRate = .3;
-            var target = [this.targetX(creature), this.targetY(creature), this.targetAngle(creature)];
-            creature.network.propagate(learningRate, target);
-
-            // draw
-            creature.draw();
+            creature.update();
         }.bind(this));
     },
 
@@ -155,29 +136,12 @@ World.prototype = {
     },
     width: function () {
         return this.renderer.view.width;
-    },
-
-    targetX: function (creature) {
-        var cohesion = creature.cohesion(this.creatures);
-        return cohesion.x / this.width();
-    },
-
-    targetY: function (creature) {
-        var cohesion = creature.cohesion(this.creatures);
-        return cohesion.y / this.height();
-    },
-
-    targetAngle: function (creature) {
-        var alignment = creature.align(this.creatures);
-        return (alignment.angle() + Math.PI) / (Math.PI * 2);
-    },
+    }
 };
 (function () {
     document.addEventListener('deviceready', function () {
         PIXI.loader
             .add([
-                "/res/icons/android/icon-36-ldpi.png",
-                "/res/icons/android/icon-48-mdpi.png",
                 "/res/icons/android/icon-96-xhdpi.png"
             ])
             .load(function () {
